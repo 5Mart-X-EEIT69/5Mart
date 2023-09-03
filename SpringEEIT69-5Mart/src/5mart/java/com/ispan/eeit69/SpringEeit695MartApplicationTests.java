3553a8b328f30137cc82fd725d19a0eb117ea15b
Jacky0400143 <jacky0400143+Github@gmail.com>
2023-09-03 17:37:05 +0800

    20230903-Initial commit

---
+++ SpringEEIT69-5Mart/src/5mart/java/com/ispan/eeit69/SpringEeit695MartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ispan.eeit69;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEeit695MartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.ispan.eeit69.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("welcome","歡迎來到Spring Boot的世界");
+		return "test1";
+	}
+	
+	@GetMapping("/test2")
+	public String test2(Model model) {
+		model.addAttribute("welcome","歡迎來到Spring Boot的世界");
+		return "test2";
+	}
+	
+	@GetMapping("/test3")
+	public String test3(Model model) {
+		model.addAttribute("welcome","歡迎來到Spring Boot的世界");
+		return "test3";
+	}
+}
