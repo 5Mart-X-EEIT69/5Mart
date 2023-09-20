e353d6f1e06cc935c4b9fe83798a670000fc27ab
User <asdasd789tw@gmail.com>
2023-09-20 16:56:39 +0800

    09200456

--- SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/dao/unitRepository.java
+++ SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/dao/unitRepository.java
@@ -2,6 +2,6 @@ package com.ispan.eeit69.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface unitRepository extends JpaRepository<T, ID> {
+public interface unitRepository {
 
 }
