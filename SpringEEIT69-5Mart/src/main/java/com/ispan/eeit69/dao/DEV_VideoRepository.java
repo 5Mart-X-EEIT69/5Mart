b101558c933760a17da4a808d81fea94b51b0311
WANG T.C <75102529+Jacky0400143@users.noreply.github.com>
2023-09-27 23:40:01 +0800

    Update DEV_VideoRepository.java

--- SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/dao/DEV_VideoRepository.java
+++ SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/dao/DEV_VideoRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ispan.eeit69.model.Video;
+import com.ispan.eeit69.model.DEV_Video;
 
 @Repository
 public interface VideoRepository extends JpaRepository<Video, Integer>{
