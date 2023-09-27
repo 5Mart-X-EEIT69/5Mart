660fef39aaa1f565ee2ed72d4740ebe0f335a6d6
WANG T.C <75102529+Jacky0400143@users.noreply.github.com>
2023-09-27 23:44:59 +0800

    Create DEV_VideoService.java

---
+++ SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/service/DEV_VideoService.java
@@ -0,0 +1,16 @@
+package com.ispan.eeit69.service;
+
+import java.util.List;
+
+import com.ispan.eeit69.model.DEV_Video;
+
+public interface VideoService {
+	void save(Video video);
+	Video update(Video video);
+	Video findById(Integer id);
+	List<Video> findAll();
+	void deleteById(Integer id);//大禮包抄來的基本功能
+	
+	 Video findByUuid(String uuid);  // 新增的方法，用來找UUID
+
+}
