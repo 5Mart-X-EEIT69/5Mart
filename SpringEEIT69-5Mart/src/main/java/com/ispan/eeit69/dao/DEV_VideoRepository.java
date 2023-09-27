04c901ab409affd25e3cb564b24a7c277388d1e7
WANG T.C <75102529+Jacky0400143@users.noreply.github.com>
2023-09-27 23:39:23 +0800

    Create DEV_VideoRepository.java

---
+++ SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/dao/DEV_VideoRepository.java
@@ -0,0 +1,18 @@
+package com.ispan.eeit69.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ispan.eeit69.model.Video;
+
+@Repository
+public interface VideoRepository extends JpaRepository<Video, Integer>{
+	
+	//自定義的一個方法。Spring Data JPA 根據方法名自動實現 SQL 查詢。
+	//findByUuid 會自動轉成一個 SQL 查詢，用來根據 UUID 字段找到對應的 Video 實體。
+	//返回一個 Optional<Video>，這意味著查詢的結果可能是空。
+	
+	 Optional<Video> findByUuid(String uuid);  // 用來找UUID的方法
+}
