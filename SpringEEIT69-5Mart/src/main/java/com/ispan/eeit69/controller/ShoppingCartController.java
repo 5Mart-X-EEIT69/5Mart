b11d48ded07acfbacbcfac06312b051ebe24b46b
chengtungtung <hsintung.cheng@gmail.com>
2023-09-30 12:24:06 +0800

    整理程式碼_0930_12:24

--- SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/controller/ShoppingCartController.java
+++ SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/controller/ShoppingCartController.java
@@ -21,54 +21,54 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.websocket.Session;
 
 @Controller
-@SessionAttributes({"memberdata"})
+@SessionAttributes({ "memberdata" })
 public class ShoppingCartController {
 	private static Logger log = LoggerFactory.getLogger(ShoppingCartController.class);
-	
+
 	CourseService courseService;
 	HttpSession httpSession;
-	
-	
 
 	public ShoppingCartController(CourseService courseService, HttpSession httpSession) {
 		this.courseService = courseService;
 		this.httpSession = httpSession;
 	}
 
-
-
 	@GetMapping("/addtocart")
 	public String addtocart(@RequestParam("id") String id, Model model) {
 //		member member =  (member)model.getAttribute("memberdata");
-		member member =  (member)httpSession.getAttribute("member");
-		if(member == null) {
+		member member = (member) httpSession.getAttribute("member");
+		if (member == null) {
 			log.info("session裡沒有memberdata物件(會員資料)，跳轉首頁讓使用者登入");
 			System.out.println("id值 " + id);
 			return "homePage";
 		}
 //		Map<Integer, Course> cart = (Map<Integer, Course>)httpSession.getAttribute("ShoppingCart");
-		ShoppingCart cart = (ShoppingCart)httpSession.getAttribute("ShoppingCart");
-		if(cart == null) {
+		ShoppingCart cart = (ShoppingCart) httpSession.getAttribute("ShoppingCart");
+		if (cart == null) {
 			log.info("session裡沒有ShoppingCart物件，新建了ShoppingCart物件");
 			ShoppingCart ShoppingCart = new ShoppingCart();
 			Integer intId = Integer.parseInt(id);
 			Course cource = courseService.findById(intId);
-			ShoppingCart.addToCart(intId,cource);
+			ShoppingCart.addToCart(intId, cource);
 			Map<Integer, Course> mycart = ShoppingCart.getContent();
 //			model.addAttribute("ShoppingCart", cart);
 			httpSession.setAttribute("mycart", mycart);
 			httpSession.setAttribute("ShoppingCart", ShoppingCart);
-			
-		}else {
+		} else {
 			Integer intId = Integer.parseInt(id);
 			Course cource = courseService.findById(intId);
-			cart.addToCart(intId,cource);
+			cart.addToCart(intId, cource);
 			Map<Integer, Course> mycart = cart.getContent();
 //			model.addAttribute("ShoppingCart", cart);
 			httpSession.setAttribute("mycart", mycart);
-			
 		}
-		
 		return "redirect:/courseDetail?id=" + id;
 	}
+	
+	
+	
+	
+	
+	
+	
 }
