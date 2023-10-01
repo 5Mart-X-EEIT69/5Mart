7288f3f56b74d7bd9091b02829ef3f65d27a18a9
WANG T.C <75102529+Jacky0400143@users.noreply.github.com>
2023-10-01 12:44:19 +0800

    Feat:	OneToOne between AccountSetting and Member model

--- SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/model/AccountSetting.java
+++ SpringEEIT69-5Mart/src/main/java/com/ispan/eeit69/model/AccountSetting.java
@@ -1,3 +1,5 @@
+package com.ispan.eeit69.model;
+
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
@@ -19,10 +21,58 @@ public class AccountSetting implements Serializable {
 
     private String preferredLanguage; // 希望的網頁語言
     private Boolean showRealName;     // 是否對外顯示真實姓名
+    private Boolean receiveMarketingInfo;  // 是否願意接收行銷資訊
 
     @OneToOne()
     @JoinColumn(name = "member_id")
     private member member; // 對應的 member
-
+    
+    
     // getters and setters
+    
+
+	public Integer getSettingId() {
+		return settingId;
+	}
+
+	public void setSettingId(Integer settingId) {
+		this.settingId = settingId;
+	}
+
+	public String getPreferredLanguage() {
+		return preferredLanguage;
+	}
+
+	public void setPreferredLanguage(String preferredLanguage) {
+		this.preferredLanguage = preferredLanguage;
+	}
+
+	public Boolean getShowRealName() {
+		return showRealName;
+	}
+
+	public void setShowRealName(Boolean showRealName) {
+		this.showRealName = showRealName;
+	}
+
+	public Boolean getReceiveMarketingInfo() {
+		return receiveMarketingInfo;
+	}
+
+	public void setReceiveMarketingInfo(Boolean receiveMarketingInfo) {
+		this.receiveMarketingInfo = receiveMarketingInfo;
+	}
+
+	public member getMember() {
+		return member;
+	}
+
+	public void setMember(member member) {
+		this.member = member;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 }
